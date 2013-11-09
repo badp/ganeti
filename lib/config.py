c4c4df644b2159dfad40f3bbda5ad74d7320ffa5
Santi Raffa <rsanti@google.com>
2013-11-09 22:17:02 +0100

    Gluster: add userspace access support
    Add support for the QEMU gluster: protocol. Also change the access
    mode routines so they check the access parameter for all templates.

--- lib/config.py
+++ lib/config.py
@@ -678,12 +678,13 @@ class ConfigWriter(object):
             constants.NDS_PARAMETER_TYPES)
     _helper_ipolicy("cluster", cluster.ipolicy, True)
 
-    if constants.DT_RBD in cluster.diskparams:
-      access = cluster.diskparams[constants.DT_RBD][constants.RBD_ACCESS]
+    for disk_template in cluster.diskparams:
+      access = cluster.diskparams[disk_template].get(constants.RBD_ACCESS,
+                                                     constants.DISK_KERNELSPACE)
       if access not in constants.DISK_VALID_ACCESS_MODES:
         result.append(
           "Invalid value of '%s:%s': '%s' (expected one of %s)" % (
-            constants.DT_RBD, constants.RBD_ACCESS, access,
+            disk_template, constants.RBD_ACCESS, access,
             utils.CommaJoin(constants.DISK_VALID_ACCESS_MODES)
           )
         )
